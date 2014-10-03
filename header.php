e5de28c3de7bc3cdeaafedef520a4fea30ebb1c7
ahmed <ahmed.ahmed1991@gmail.com>
2014-10-03 07:12:11 -0400

    try

--- header.php
+++
@@ -1,9 +0,0 @@
-<head>
-<title> project2</title>
-<meta charset='utf-8'>
-
-<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-<meta name='viewport' content='width=device-width, initial-scale=1'>
-<link rel="stylesheet" href='//netdna.bootstrapcdn.com/bootswatch/3.0.3/css/bootstrap.min.css'>
-<link rel="stylesheet" href="css/styles.css">
-</head>
--- index.php
+++ index.php
@@ -7,9 +7,17 @@ ini_set('display_errors', 1);
 
 <!DOCTYPE html>
 <html>
-<?php include('header.php'); ?>
-<boody>
-	<?include('content.html'); ?>
+  <head>
+<title> project2</title>
+<meta charset='utf-8'>
+
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<link rel="stylesheet" href='//netdna.bootstrapcdn.com/bootswatch/3.0.3/css/bootstrap.min.css'>
+<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<?php include('content.html'); ?>
 
 </body>
 <script type="text/javascript"></script>
